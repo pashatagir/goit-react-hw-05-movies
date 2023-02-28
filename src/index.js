a6a500c098eca7b3b09f60d7a53433ed2bb36e1b
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2023-02-28 10:45:41 +0200

    added bzsename

--- src/index.js
+++ src/index.js
@@ -8,10 +8,7 @@ import { GlobalStyles, theme } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter
-      basename="/
-goit-react-hw-05-movies"
-    >
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyles} />
         <App />
